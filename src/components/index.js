7c6c7a4d03cf00a1b1aaa357d5518da7d12688c0
thekevinkun <developer.kevinkun@gmail.com>
2024-11-07 02:45:06 +0800

    Added Embla Carousel for Blog

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Nav from "./Nav/Nav";
 import Button from "./Button/Button";
 import FaqItem from "./FaqItem/FaqItem";
-import EmblaCarousel from "./Embla/EmblaCarousel";
+import EmblaCarouselHero from "./EmblaHero/EmblaCarouselHero";
+import EmblaCarouselBlog from "./EmblaBlog/EmblaCarouselBlog";
 
 export {
     Nav,
     Button,
     FaqItem,
-    EmblaCarousel
+    EmblaCarouselHero,
+    EmblaCarouselBlog
 }
